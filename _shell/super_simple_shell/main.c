1c4f201e87a875371c09191eea3b077603fa1aa4
Andrew Maina <drew29799@gmail.com>
2023-04-19 08:09:07 -0700

    Feature: Add user shell prompt and read input
    prompt prints a POS1 charster to ask for user input.
    getline() is used to capture input from the standard input and write to the 'args' buffer.
    The user input is then printed back to the user along with the number of bytes read.
    Function definitions and library imports are in the shell.h header file

--- _shell/super_simple_shell/main.c
+++ _shell/super_simple_shell/main.c
@@ -12,11 +12,35 @@ int main(void)
 	// Frontend
 	// create infinite loop
 	// prompt user
-	char *args[] = { "/bin/ls", "-l", "/tmp", NULL };
-	pid_t pid;
-	int i  = 0, child_status = 0;
+	// get user input (getline) - store in buffer
+	// tokenize command input into tokens (program name and arguments)
+	// Search program in PATH
 
-	while (i++ < 5)
+	// Backend:
+	// DEtermine type of command execution type - single, sequential, pipe, &&, ||
+	// Create process for execting jobs
+	// Execute commands - execve(program name, arg_list, env_arr)
+/*	char *args[] = { "/bin/ls", "-l", "/tmp", NULL }; */
+	char *args = NULL;
+	char **tokens = NULL;
+	size_t args_len = 0;
+	size_t toks_len = 0;
+	int i  = 0, child_status = 0;
+	pid_t pid;
+
+	while (1)
+	{
+		prompt();
+
+		while((args_len = getline(&args, &args_len, stdin)) != -1)
+		{
+			printf("Number of bytes read %zd\n", args_len);
+			printf("Input: %s\n", args);
+			break;
+		}
+	}
+
+/*	while (i++ < 5)
 	{
 		pid = fork();
 		if (pid == -1)
@@ -38,14 +62,6 @@ int main(void)
 			wait(&child_status);
 		}
 	}
-	// get user input (getline) - store in buffer
-	// tokenize command input into tokens (program name and arguments)
-	// Search program in PATH
-
-	// Backend:
-	// DEtermine type of command execution type - single, sequential, pipe, &&, ||
-	// Create process for execting jobs
-	// Execute commands - execve(program name, arg_list, env_arr)
-
+*/
 	return (EXIT_SUCCESS);
 }
---
+++ _shell/super_simple_shell/shell.h
@@ -0,0 +1,12 @@
+#ifndef __SHELL_PRACTICE__H
+#define __SHELL_PRACTICE__H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+void prompt(void);
+
+#endif /* #ifndef __SHELL_PRACTICE__H */
